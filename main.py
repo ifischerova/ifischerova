3ebf74b7e05e92da162716877480eacb2f980df1
Ivuska <fischerova.ivka@gmail.com>
2022-10-21 13:23:41 +0200

    Redirects to pages with Flask

--- main.py
+++ main.py
@@ -8,5 +8,17 @@ def get_main_page():
     # I need to specify the debug mode for the template "to know" about this mode.   
     return render_template('index.html', debug=app.debug)
 
+@app.route('/blog')
+def get_blog_page(): 
+    return render_template('blog.html', debug=app.debug)
+
+@app.route('/projects')
+def get_projects_page():   
+    return render_template('projects.html', debug=app.debug)
+
+@app.route('/about')
+def get_about_page():   
+    return render_template('about.html', debug=app.debug)
+
 if __name__ == '__main__':
     app.run()
