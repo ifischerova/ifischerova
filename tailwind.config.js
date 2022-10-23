2be7687277528193cd1a4b7b4227fd038fcffac8
Ivuska <fischerova.ivka@gmail.com>
2022-10-23 14:32:24 +0200

    Breakpoints added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
     content: ["templates/*.{html,js}"],
     theme: {
+    screens: {
+        'sm': '600px',
+
+        'md': '900px',
+  
+        'lg': '1200px',
+  
+        'xl': '1440px',
+        },
       extend: {},
     },
     plugins: [],
