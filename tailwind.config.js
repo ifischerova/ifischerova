e0d7fbb3f45bc92b891bbcb9fc6f19843c6dba2c
Ivuska <fischerova.ivka@gmail.com>
2022-10-24 21:25:07 +0200

    POC of colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,24 @@
 module.exports = {
     content: ["templates/*.{html,js}"],
     theme: {
-    screens: {
-        'sm': '600px',
+      screens: {
+          'sm': '600px',
 
-        'md': '900px',
-  
-        'lg': '1200px',
-  
-        'xl': '1440px',
-        },
-      extend: {},
+          'md': '900px',
+    
+          'lg': '1200px',
+    
+          'xl': '1440px',
+          },
+      extend: {
+        colors: {
+          'silver-begin':'#2E4057',
+          'silver-end':'#DBCBD8'
+        }
+      },
     },
+      fontFamily: {
+        'sans': ['Open Sans', 'sans-serif'],
+      },
     plugins: [],
   }
