a6be025cd6dfe7d31efb1e7b2d17659890d90e94
Ivuska <fischerova.ivka@gmail.com>
2023-01-19 14:50:35 +0100

    Scroll to top button.

--- _site/js/index.js
+++ _site/js/index.js
@@ -6,3 +6,20 @@ function closeHamburgerMenu() {
     toggleIcon();
     this.document.getElementById('toggle').checked = false;
 };
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById('top-button').style.display = "block";
+    } else {
+        document.getElementById('top-button').style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
