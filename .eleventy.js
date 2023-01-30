febeb4801be80c747454c7456e7dc28ea5258d56
Ivuska <fischerova.ivka@gmail.com>
2023-01-30 22:10:41 +0100

    Minify CSS in prod build.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
 
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy({"_build/css/*.css": "css"});
 
   eleventyConfig.addPassthroughCopy("js");
 
